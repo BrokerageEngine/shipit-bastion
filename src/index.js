7e5d0351d84c26f6eb11ade76412deea1c3aaf6a
Dirk Elmendorf <dirk@brokerageengine.com>
2019-03-06 17:04:51 -0600

    Rolling back to previous event

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ const extendShipit = require("./extendShipit");
 module.exports = shipit => {
     extendShipit(shipit);
   sshTask(shipit);
-   shipit.on("init:after_ssh_pool", async () => {
+/*   shipit.on("init:after_ssh_pool", async () => {
     await shipit.start("ssh:setup_bastion");
    });
-	/*
-	 * This is only required for older versions of shipit-cli
+   */
   shipit.on("task_start", async (event) => {
     if (shipit.pool && 
       !shipit.pool.isProxied 
@@ -17,6 +16,4 @@ module.exports = shipit => {
       await shipit.start("ssh:setup_bastion");
     }
   });
-
-    */
 };
